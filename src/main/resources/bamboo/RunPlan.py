a304b3d9879643cbd4197dc21ec95478dd076e31
droberts2013 <droberts@xebialabs.com>
2015-03-11 11:23:39 -0400

    Enhance script with additional output variables buildState and state; use pretty start and complete dates.

--- src/main/resources/bamboo/RunPlan.py
+++ src/main/resources/bamboo/RunPlan.py
@@ -1,7 +1,5 @@
-import datetime
 import sys
 import time
-
 import com.xhaus.jyson.JysonCodec as json
 
 print "Executing RunPlan.py\n"
@@ -23,20 +21,43 @@ def checkSuccessful(brkey):
 	response = request.get('/rest/api/latest/result/' + brkey, contentType=contentType, headers=headers)
 	return json.loads(response.response)['successful']
 
-print "Starting Bamboo job at " + str(datetime.datetime.isoformat(datetime.datetime.now())) + "\n"
+def getState(brkey):
+	request = HttpRequest(bambooServer)
+	response = request.get('/rest/api/latest/result/' + brkey, contentType=contentType, headers=headers)
+	return json.loads(response.response)['state']	
+
+def getBuildState(brkey):
+	request = HttpRequest(bambooServer)
+	response = request.get('/rest/api/latest/result/' + brkey, contentType=contentType, headers=headers)
+	return json.loads(response.response)['buildState']	
+
+def getPrettyBuildStartedTime(brkey):
+	request = HttpRequest(bambooServer)
+	response = request.get('/rest/api/latest/result/' + brkey, contentType=contentType, headers=headers)
+	return json.loads(response.response)['prettyBuildStartedTime']	
+
+def getPrettyBuildCompletedTime(brkey):
+	request = HttpRequest(bambooServer)
+	response = request.get('/rest/api/latest/result/' + brkey, contentType=contentType, headers=headers)
+	return json.loads(response.response)['prettyBuildCompletedTime']	
+
+print "Executing RunPlan.py at " + "\n"
 request = HttpRequest(bambooServer)
 response = request.post('/rest/api/latest/queue/' + projPlanKey, '{}', contentType=contentType, headers=headers)
 result = json.loads(response.response)
 buildNumber = result['buildNumber']
 print 'Build number is ' + str(buildNumber) + '\n'
-
 brkey = result['buildResultKey']
+print "Build job started at " + getPrettyBuildStartedTime(brkey) + "\n"
+
 while (not checkFinished(brkey)):
-	#print "Sleeping for 5 seconds at " + str(datetime.datetime.isoformat(datetime.datetime.now())) + "\n"
 	time.sleep(5)
 	
+prettyBuildCompletedTime = getPrettyBuildCompletedTime(brkey)
 if checkSuccessful(brkey):
-	print "Build job completed successfully at " + str(datetime.datetime.isoformat(datetime.datetime.now())) + "\n"
+	print "Build job completed successfully at " + prettyBuildCompletedTime + "\n"
 else:
-	print "Build job failed at " + str(datetime.datetime.isoformat(datetime.datetime.now())) + "\n"
+	print "Build job failed at " + prettyBuildCompletedTime + "\n"
 
+buildState = getBuildState(brkey)
+state = getState(brkey)
