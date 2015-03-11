5341e3c39b1b690d472412ae721595f9e29bf69b
droberts2013 <droberts@xebialabs.com>
2015-03-11 11:50:50 -0400

    RunPlan.py script improvements

--- src/main/resources/bamboo/RunPlan.py
+++ src/main/resources/bamboo/RunPlan.py
@@ -11,12 +11,12 @@ if bambooServer is None:
 contentType = 'application/json'
 headers = {'accept' : 'application/json'}
 
-def checkFinished(brkey):
+def finished(brkey):
 	request = HttpRequest(bambooServer)
 	response = request.get('/rest/api/latest/result/' + brkey, contentType=contentType, headers=headers)
 	return json.loads(response.response)['finished']
 
-def checkSuccessful(brkey):
+def successful(brkey):
 	request = HttpRequest(bambooServer)
 	response = request.get('/rest/api/latest/result/' + brkey, contentType=contentType, headers=headers)
 	return json.loads(response.response)['successful']
@@ -41,20 +41,19 @@ def getPrettyBuildCompletedTime(brkey):
 	response = request.get('/rest/api/latest/result/' + brkey, contentType=contentType, headers=headers)
 	return json.loads(response.response)['prettyBuildCompletedTime']	
 
-print "Executing RunPlan.py at " + "\n"
 request = HttpRequest(bambooServer)
 response = request.post('/rest/api/latest/queue/' + projPlanKey, '{}', contentType=contentType, headers=headers)
 result = json.loads(response.response)
 buildNumber = result['buildNumber']
 print 'Build number is ' + str(buildNumber) + '\n'
 brkey = result['buildResultKey']
-print "Build job started at " + getPrettyBuildStartedTime(brkey) + "\n"
 
-while (not checkFinished(brkey)):
+while (not finished(brkey)):
 	time.sleep(5)
 	
+print "Build job started at " + getPrettyBuildStartedTime(brkey) + "\n"
 prettyBuildCompletedTime = getPrettyBuildCompletedTime(brkey)
-if checkSuccessful(brkey):
+if successful(brkey):
 	print "Build job completed successfully at " + prettyBuildCompletedTime + "\n"
 else:
 	print "Build job failed at " + prettyBuildCompletedTime + "\n"
