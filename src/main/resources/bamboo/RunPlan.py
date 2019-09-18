ec7d38bd4d03a987325c6e0d0362e0ffb36281bb
Rick Broker <rbroker@blackpugsoftware.com>
2019-09-17 21:24:56 -0400

    Updates to make `username` & `password` overrideable

--- src/main/resources/bamboo/RunPlan.py
+++ src/main/resources/bamboo/RunPlan.py
@@ -1,39 +1,40 @@
 #
-# THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
-# FOR A PARTICULAR PURPOSE. THIS CODE AND INFORMATION ARE NOT SUPPORTED BY XEBIALABS.
+#Copyright 2019 XEBIALABS
 #
+#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.#
+#
 import sys
 import time
 import com.xhaus.jyson.JysonCodec as json
 
-print "Executing RunPlan.py ver 2015Mar12-2\n"
+print "Executing RunPlan.py\n"
 
 if bambooServer is None:
 	print "No server provided."
 	sys.exit(1)
-	
+
 contentType = 'application/json'
 headers = {'accept' : 'application/json'}
 
 def finished(brkey):
-	request = HttpRequest(bambooServer)
 	response = request.get('/rest/api/latest/result/' + brkey, contentType=contentType, headers=headers)
 	return json.loads(response.response)['finished']
 
 def successful(brkey):
-	request = HttpRequest(bambooServer)
 	response = request.get('/rest/api/latest/result/' + brkey, contentType=contentType, headers=headers)
 	return json.loads(response.response)['successful']
 
 def getStatesAndTimes(brkey):
-	request = HttpRequest(bambooServer)
 	response = request.get('/rest/api/latest/result/' + brkey, contentType=contentType, headers=headers)
 	jsonData = json.loads(response.response)
 	return (jsonData['buildState'], jsonData['state'], jsonData['prettyBuildStartedTime'], jsonData['prettyBuildCompletedTime'])
 
-request = HttpRequest(bambooServer)
+credentials = CredentialsFallback(bambooServer, username, password).getCredentials()
+request = HttpRequest(bambooServer, credentials['username'], credentials['password'])
 response = request.post('/rest/api/latest/queue/' + projPlanKey, '{}', contentType=contentType, headers=headers)
 result = json.loads(response.response)
 buildNumber = result['buildNumber']
@@ -42,7 +43,7 @@ brkey = result['buildResultKey']
 
 while (not finished(brkey)):
 	time.sleep(5)
-	
+
 (buildState, state, prettyBuildStartedTime, prettyBuildCompletedTime) = getStatesAndTimes(brkey)
 
 print "Build job started at " + prettyBuildStartedTime + "\n"
--- src/main/resources/bamboo/TriggerDeployment.py
+++ src/main/resources/bamboo/TriggerDeployment.py
@@ -1,7 +1,11 @@
 #
-# THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
-# FOR A PARTICULAR PURPOSE. THIS CODE AND INFORMATION ARE NOT SUPPORTED BY XEBIALABS.
+#Copyright 2019 XEBIALABS
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.#
 #
 
 import sys
@@ -13,14 +17,13 @@ print "Executing triggerBambooDeployment.py\n"
 if bambooServer is None:
   print "No server provided."
   sys.exit(1)
-	
+
 contentType = 'application/json'
 headers = {'accept' : 'application/json'}
 
 def getProjectId(projectName):
   print "Executing getProjectId() with projectName %s\n" % projectName
-  request = HttpRequest(bambooServer)
-  response = request.get('rest/api/latest/deploy/project/all', contentType=contentType, headers=headers)	
+  response = request.get('rest/api/latest/deploy/project/all', contentType=contentType, headers=headers)
   for item in json.loads(response.response):
     if item['name'] == projectName:
       return item['id']
@@ -29,7 +32,6 @@ def getProjectId(projectName):
 
 def getEnvironmentId(projectId, environmentName):
   print "Executing getEnvironmentId() with projectId %s and environmentName %s\n" % (projectId, environmentName)
-  request = HttpRequest(bambooServer)
   response = request.get('rest/api/latest/deploy/project/%s' % projectId, contentType=contentType, headers=headers)
   for item in json.loads(response.response)['environments']:
     if item['name'] == environmentName:
@@ -38,8 +40,7 @@ def getEnvironmentId(projectId, environmentName):
   sys.exit(1)
 
 def getVersionId(projectId, versionName):
-  print "Executing getVersionId() with projectId %s and versionName %s\n" % (projectId, versionName) 
-  request = HttpRequest(bambooServer)
+  print "Executing getVersionId() with projectId %s and versionName %s\n" % (projectId, versionName)
   response = request.get('rest/api/latest/deploy/project/%s/versions' % projectId, contentType=contentType, headers=headers)
   for item in json.loads(response.response)['versions']:
     if item['name'] == versionName:
@@ -49,12 +50,13 @@ def getVersionId(projectId, versionName):
 
 def triggerDeployment(environmentId, versionId):
   print "Executing triggerDeployment() with environmentId %s and versionId %s\n" % (environmentId, versionId)
-  request = HttpRequest(bambooServer)
   response = request.post('rest/api/latest/queue/deployment/?environmentId=%s&versionId=%s' % (environmentId, versionId), '{}', contentType=contentType, headers=headers)
   result = json.loads(response.response)
   print (result['deploymentResultId'], result['link']['href'])
   return (result['deploymentResultId'], result['link']['href'])
 
+credentials = CredentialsFallback(bambooServer, username, password).getCredentials()
+request = HttpRequest(bambooServer, credentials['username'], credentials['password'])
 projectId = getProjectId(projectName)
 environmentId = getEnvironmentId(projectId, environmentName)
 versionId = getVersionId(projectId, versionName)
