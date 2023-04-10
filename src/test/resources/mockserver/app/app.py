cd64e2b12e5a9924cc968e0e0931a95b2403e461
droberts2013 <droberts@xebialabs.com>
2023-04-10 15:22:45 -0400

    Update copyright year and tag

--- src/test/resources/mockserver/app/app.py
+++ src/test/resources/mockserver/app/app.py
@@ -1,6 +1,6 @@
 #!flask/bin/python
 #
-# Copyright 2022 XEBIALABS
+# Copyright 2023 XEBIALABS
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #
