733b30a87dc4ffd6e5f69d1b3ecbbfe38247624d
droberts2013 <droberts@xebialabs.com>
2023-07-11 01:16:10 -0400

    Add option to include log lines with each iteration

--- src/main/resources/bamboo/TriggerDeployment.wait-for-deployment.py
+++ src/main/resources/bamboo/TriggerDeployment.wait-for-deployment.py
@@ -8,6 +8,7 @@
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.#
 #
 
+import re
 import sys
 import time
 import com.xhaus.jyson.JysonCodec as json
@@ -24,27 +25,66 @@ headers = {'accept' : 'application/json'}
 credentials = CredentialsFallback(bambooServer, username, password).getCredentials()
 request = HttpRequest(bambooServer, credentials['username'], credentials['password'])
 
-def getDeploymentStatus():
+logFileRefPattern = re.compile(".*/(deployment-download.*)")
+
+def getDeploymentStatus(deploymentResultId, maxLogLinesPerQuery):
     print "Executing getDeploymentStatus()\n"
-    response = request.get('rest/api/latest/deploy/result/%s' % str(deploymentResultId), contentType=contentType, headers=headers)
-    if response.isSuccessful():
-        result = json.loads(response.response)
-        return (result['lifeCycleState'], result['deploymentState'], result['logFiles'][0])
+    if maxLogLinesPerQuery > 0:
+        response = request.get('rest/api/latest/deploy/result/%s?includeLogs=true&max-result=%s' % (str(deploymentResultId), str(maxLogLinesPerQuery)), contentType=contentType, headers=headers)
+        if response.isSuccessful():
+            result = json.loads(response.response)
+            return (result['lifeCycleState'], result['deploymentState'], result['logFiles'][0], result['logEntries'])
+        else:
+            print "Error: HTTP status code %s" % str(response.getStatus())
+            sys.exit(1)
     else:
-        print "Error: HTTP status code %s" % str(response.getStatus())
-        sys.exit(1)
+        response = request.get('rest/api/latest/deploy/result/%s' % str(deploymentResultId), contentType=contentType, headers=headers)
+        if response.isSuccessful():
+            result = json.loads(response.response)
+            return (result['lifeCycleState'], result['deploymentState'], result['logFiles'][0], None)
+        else:
+            print "Error: HTTP status code %s" % str(response.getStatus())
+            sys.exit(1)
+
+def printLogEntries(logEntries):
+    if 'size' in logEntries.keys():
+        print "Log size is %d" % logEntries['size']
+    if 'start-index' in logEntries.keys():
+        print "Start index is %d" % logEntries['start-index']
+    if 'max-result' in logEntries.keys():
+        print "Max result is %d" % logEntries['max-result']
+    for logEntry in logEntries['logEntry']:
+        print "%s %s" % (logEntry['formattedDate'], logEntry['unstyledLog'])
+
+#
+# Example: "http://54.157.13.243:8085/deployment-download/1015809/build_logs/950273-1015809-1146883.log"
+#
+def printLogFile(request, logFileRef):
+    m = logFileRefPattern.match(logFileRef)
+    if m:
+        logResponse = request.get(m.group(1))
+        if logResponse.isSuccessful():
+            print logResponse.response
 
 projectId = projectId or foundProjectId
 
-(lifeCycleState, deploymentState, zerothLogFileRef) = getDeploymentStatus()
+(lifeCycleState, deploymentState, zerothLogFileRef, logEntries) = getDeploymentStatus(deploymentResultId, maxLogLinesPerQuery)
 
 if lifeCycleState == "FINISHED":
     if deploymentState == "SUCCESS":
         print "Deployment has completed successfully."
+        print zerothLogFileRef
+        printLogFile(request, zerothLogFileRef)
     elif deploymentState in ("FAILED", "UNKNOWN"):
         print "Error: lifeCycleState is %s and deploymentState is %s" % (lifeCycleState, deploymentState)
+        print zerothLogFileRef
+        printLogFile(request, zerothLogFileRef)
         sys.exit(1)
 elif lifeCycleState == "IN_PROGRESS":
+    if maxLogLinesPerQuery > 0:
+        if logEntries:
+            printLogEntries(logEntries)
+	task.setStatusLine("Triggered Deployment %s" % lifeCycleState)
     task.schedule("bamboo/TriggerDeployment.wait-for-deployment.py", 30)
 else:
     print "Error: Invalid lifeCycleState %s with deploymentState %s" % (lifeCycleState, deploymentState)
